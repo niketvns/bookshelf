ae964ecbabd014b425513950d619c2f2c7128b2a
NIKET KUMAR MISHRA <niketvns97@gmail.com>
2023-06-18 01:51:31 +0530

    update

--- src/components/Plus.jsx
+++ src/components/Plus.jsx
@@ -7,7 +7,7 @@ const Plus = () => {
     return (
         <div className={'bg-green-600 fixed bottom-8 right-8 p-4 rounded-full cursor-pointer text-white border-2 border-white/30'}
              onClick={() => navigate('/search')}>
-            <AiOutlinePlus/>
+            <AiOutlinePlus className={'text-2xl font-bold'}/>
         </div>
     );
 };
