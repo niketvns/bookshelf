025ff193259c05f5a112013d215cdbdf8086cd2e
NIKET KUMAR MISHRA <niketvns97@gmail.com>
2023-06-17 22:39:42 +0530

    update

--- src/db/books.js
+++ src/db/books.js
@@ -43,42 +43,42 @@ export const allBooks = [
     },
     {
         id: uuid(),
-        image: "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
-        title: "Atomic Habits",
+        image: "https://m.media-amazon.com/images/I/41sjdgeKJrL._SX324_BO1,204,203,200_.jpg",
+        title: "Rich Dad Poor Dad",
         rating: 4.5,
-        writer: "James Clear",
+        writer: "Robert T. Kiyosaki",
         category: 'read'
     },
     {
         id: uuid(),
-        image: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1588286863i/634583.jpg",
-        title: "Wings of fire",
+        image: "https://m.media-amazon.com/images/I/41W-o6xu2bL._SX323_BO1,204,203,200_.jpg",
+        title: "Do It Today",
         rating:4.8,
-        writer: "A. P. J. Abdul Kalam and Arun Tiwari",
+        writer: "Darius Foroux",
         category: 'reading'
     },
     {
         id: uuid(),
-        image: "https://m.media-amazon.com/images/I/510ASCR9goL._SX394_BO1,204,203,200_.jpg",
-        title: "Twelfth Fail",
+        image: "https://m.media-amazon.com/images/I/41RwQlo446L._SX329_BO1,204,203,200_.jpg",
+        title: "Energize Your Mind",
         rating: 4.2,
-        writer: "Anurag Pathak",
+        writer: "Gaur Gopal Das",
         category: 'wantsToRead'
     },
     {
         id: uuid(),
-        image: "https://m.media-amazon.com/images/I/61QLF5XF8oL._SX324_BO1,204,203,200_.jpg",
-        title: "SOPHIE'S WORLD",
+        image: "https://m.media-amazon.com/images/I/512hMmtr1HL._SX322_BO1,204,203,200_.jpg",
+        title: "Ambedkar's India",
         rating: 4.2,
-        writer: "Jostein Gaarder",
+        writer: "B.R.Ambedkar",
         category: 'reading'
     },
     {
         id: uuid(),
-        image: "https://eloquentjavascript.net/img/cover.jpg",
-        title: "Eloquent JavaScript",
+        image: "https://m.media-amazon.com/images/I/41PA2fGBGEL._SX324_BO1,204,203,200_.jpg",
+        title: "Reflections",
         rating: 4.2,
-        writer: "Marijn Haverbeke",
+        writer: "Swami Vivekananda",
         category: 'wantsToRead'
     },
 
